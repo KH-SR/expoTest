b1fc05201960274ceb0013f4ab99a0ca081d3948
KH-SR <yourname1224@gmail.com>
2020-09-25 10:31:56 +0900

    キーボードをずらす処理を追加

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  StyleSheet, View, TextInput,
+  StyleSheet, KeyboardAvoidingView, TextInput,
 } from 'react-native';
 import firebase from 'firebase';
 import CircleButton from '../elements/CircleButton';
@@ -45,7 +45,7 @@ class MemoEditScreen extends React.Component {
   render() {
     return (
     // マルチラインで複数行のTextInputを可能にする
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
         <TextInput
           style={styles.memoEditInput}
           multiline
@@ -56,7 +56,7 @@ class MemoEditScreen extends React.Component {
           name="check"
           onPress={this.handlePress.bind(this)}
         />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
